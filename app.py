62f3a384bdb3223b99735ed6ae3fa288ed12304b
johntronik <g.by.john@gmail.com>
2021-12-09 19:16:48 +0900

    add github link

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
 import base64
 import webbrowser
 from PIL import Image
+
 
 def get_table_download_link(df):
     """Generates a link allowing the data in a given panda dataframe to be downloaded
@@ -10,9 +12,11 @@ def get_table_download_link(df):
     out: href string
     """
     csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    # some strings <-> bytes conversions necessary here
+    b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="jaist_lecture_calendar.csv">Download csv file</a>'
     return href
+
 
 df = pd.read_csv('jaist-lecture.csv')
 img1 = Image.open('jl1.png')
@@ -24,10 +28,13 @@ img5 = Image.open('jl5.png')
 # 以下コンテンツ
 st.title('JAIST東京 授業カレンダー登録 2021')
 st.markdown('11/30現在のデータです')
+st.markdown(
+    '**データのミス、コメント等は[Github](https://github.com/johntronik/jaist-tokyo-lecture)にお願いします**')
 st.markdown('## 1. 授業を選択する')
-## 講義選択
-lectures = st.multiselect('', df['Subject'].unique().tolist(), ['I485F-知覚情報処理特論'])
-## 選択したデータを表示
+# 講義選択
+lectures = st.multiselect(
+    '', df['Subject'].unique().tolist(), ['I485F-知覚情報処理特論'])
+# 選択したデータを表示
 data = df[df['Subject'].isin(lectures)]
 st.table(data)
 
@@ -35,7 +42,8 @@ st.markdown('## 2. csvをダウンロードする')
 st.markdown(get_table_download_link(data), unsafe_allow_html=True)
 
 st.markdown('## 3. グーグルカレンダーを開く')
-st.markdown('[Google calendar](https://calendar.google.com/calendar/)', unsafe_allow_html=True)
+st.markdown('[Google calendar](https://calendar.google.com/calendar/)',
+            unsafe_allow_html=True)
 
 st.markdown('## 4. 新しいカレンダーを作成する')
 st.markdown('手順4は省略可能です(既にあるカレンダーにもインポートできます)')
